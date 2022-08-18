e97a7fb70b95e35df7fe13c70d8cd80c2755882f
Akin Alkan <akinalkan45@gmail.com>
2022-08-19 01:22:10 +0300

    deneme-2

--- C01.java
+++ C01.java
@@ -2,5 +2,6 @@ public class C01 {
     public static void main(String[] args) {
         System.out.println("deneme");
         System.out.println("Mustafa SAK EKLEME YAPILAN SATIRR");
+        System.out.println("deneme akin");
     }
 }
