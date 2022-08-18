29a9d3c1b8c8113e7bdad4dd446ee0e236a4cf93
Mustafa SAK <mstf6309@gmail.com>
2022-08-19 01:22:48 +0300

    ilk

--- C01.java
+++ C01.java
@@ -2,5 +2,8 @@ public class C01 {
     public static void main(String[] args) {
         System.out.println("deneme");
         System.out.println("Mustafa SAK EKLEME YAPILAN SATIRR");
+        System.out.println("Mustafa SAK EKLEME YAPILAN SATIRR22");
+        System.out.println("Mustafa SAK EKLEME YAPILAN SATIRR22");
+
     }
 }
